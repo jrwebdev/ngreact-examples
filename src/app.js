97af18a7917aed42198300164c7b0d8c800ba093
jrwebdev <web@jravenscroft.co.uk>
2016-05-18 22:02:10 +1200

    Redux example

--- src/app.js
+++ src/app.js
@@ -1,16 +1,56 @@
+import React from 'react';
 import * as angular from 'angular';
+import {combineReducers} from 'redux';
+import {connect} from 'react-redux';
+import createLogger from 'redux-logger';
+import ngRedux from 'ng-redux';
+
+const logger = createLogger();
+
+import appReducer from './state/app';
+let rootReducer = combineReducers({app: appReducer});
 
 import toggle from './components/toggle';
 
-let app = angular.module('app', [toggle]);
+class Container extends React.Component {
+    render () {
+        return <span>{this.props.toggleValue.toString()}</span>
+    }
+
+}
+
+const ConnectedContainer = connect((state, a, b) => {
+    console.debug('react', {state, a, b});
+    return {
+        toggleValue: state.app.toggleValue
+    }
+})(Container);
+
+let app = angular.module('app', [toggle, ngRedux]);
+
+app.config(($ngReduxProvider) => {
+    $ngReduxProvider.createStoreWith(rootReducer, [logger]);
+});
+
+app.directive('container', (reactDirective, $ngRedux) => {
+    return reactDirective(ConnectedContainer, null, {}, {store: $ngRedux});
+});
+
 
 app.directive('app', () => ({
     controllerAs: 'app',
-    controller () {
-        this.toggleValue = false;
+    controller ($ngRedux) {
+
+        $ngRedux.connect((state) => {
+            return {
+                toggleValue: state.app.toggleValue
+            }
+        })(this);
+
         this.onToggle = (newValue) => {
-            this.toggleValue = newValue;
+            $ngRedux.dispatch({type: 'TOGGLE'});
         }
+
     },
     template: `
         <div>Value: {{app.toggleValue}}</div>
@@ -18,7 +58,8 @@ app.directive('app', () => ({
                 on-toggle="app.onToggle(value)" 
                 true-label="True" 
                 false-label="False">
-        </toggle>        
+        </toggle>
+        <container></container>        
     `
 }));
 
